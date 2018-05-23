0c20162f3860d6999994fa85ef484205133d1fea
filipHartman <filip.hartman23@gmail.com>
2018-05-23 18:11:29 +0200

    Add boolean flag to determine if the node is the end of word

--- src/main/java/com/codecool/javatries/TrieDataNode.java
+++ src/main/java/com/codecool/javatries/TrieDataNode.java
@@ -4,6 +4,7 @@ public class TrieDataNode {
     public static final int ALPHABETIC_SIZE = 26;
     private char data;
     private TrieDataNode[] childrens;
+    boolean isWord;
     // TODO add further members as needed
 
     /**
@@ -13,6 +14,7 @@ public class TrieDataNode {
     public TrieDataNode(char data) {
         this.data = data;
         this.childrens = new TrieDataNode[ALPHABETIC_SIZE];
+        this.isWord = false;
     }
 
     public char getData() {
